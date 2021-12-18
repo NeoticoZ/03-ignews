ac30c8e12a2680d9cdab8278f9024cc4e80db860
NeoticoZ <heriksaraujodias@gmail.com>
2021-12-18 12:26:32 -0300

    :package:

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -22,7 +22,6 @@ export const config = {
   }
 }
 
-// Cria uma array com o(s) tipos que nos são interessantes
 const relevantEvents = new Set([
   'checkout.session.completed',
   'customer.subscription.updated',
@@ -31,16 +30,12 @@ const relevantEvents = new Set([
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    // Atribui o valor da {req} na função {buffer()}
     const buf = await buffer(req)
 
-    // Busca a secret no header da {req}
     const secret = req.headers['stripe-signature']
 
-    // Cria um evento que recebe a tipagem
     let event: Stripe.Event
 
-    // Tenta criar um evento
     try {
       event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
     } catch (err) {
@@ -48,10 +43,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(400).send(`Webhook error: ${err.message}`)
     }
 
-    // Se tiver dado certo a ação anterior faz a destruturação da variável {type}
     const { type } = event
 
-    // Verifica se o type é o mesmo definido no relevantEvents e chama a função {saveSubscription}
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
